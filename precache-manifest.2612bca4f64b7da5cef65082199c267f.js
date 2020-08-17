769c20e6bce589f39bd6221f64c65246968f68ac
Just A YAM Farmer <yamfinance2.0@gmail.com>
2020-08-17 00:24:18 -0400

    Updates

---
+++ precache-manifest.2612bca4f64b7da5cef65082199c267f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b225ef79f935d8fb3fea24a63ddeb6",
+    "url": "/yam-protocol/index.html"
+  },
+  {
+    "revision": "aed934cf8b5158581156",
+    "url": "/yam-protocol/static/css/main.4c3c6279.chunk.css"
+  },
+  {
+    "revision": "e3a676031951a54d9d34",
+    "url": "/yam-protocol/static/js/0.8fe6ced4.chunk.js"
+  },
+  {
+    "revision": "b042cdf671eafa775288",
+    "url": "/yam-protocol/static/js/10.15d7400e.chunk.js"
+  },
+  {
+    "revision": "5c5fdc17b19e708394f7",
+    "url": "/yam-protocol/static/js/11.b17e3e91.chunk.js"
+  },
+  {
+    "revision": "cdb6b9644b732c0f582b",
+    "url": "/yam-protocol/static/js/3.89670f0e.chunk.js"
+  },
+  {
+    "revision": "3528c20d52ee8276390269cf62243107",
+    "url": "/yam-protocol/static/js/3.89670f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2c3ba3a3dedcf5001f",
+    "url": "/yam-protocol/static/js/4.58c9b157.chunk.js"
+  },
+  {
+    "revision": "cba76d95f1bdd60480aa",
+    "url": "/yam-protocol/static/js/5.e2b9b347.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/yam-protocol/static/js/5.e2b9b347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79a332431a89f0ec7c2",
+    "url": "/yam-protocol/static/js/6.e54079d3.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/yam-protocol/static/js/6.e54079d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb92c04b8db8b76e75b",
+    "url": "/yam-protocol/static/js/7.840b1273.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/yam-protocol/static/js/7.840b1273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f1a733596a4002999d",
+    "url": "/yam-protocol/static/js/8.f1ea35ef.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/yam-protocol/static/js/8.f1ea35ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b99fe961669eff7b74",
+    "url": "/yam-protocol/static/js/9.66ab999b.chunk.js"
+  },
+  {
+    "revision": "aed934cf8b5158581156",
+    "url": "/yam-protocol/static/js/main.bc5c6457.chunk.js"
+  },
+  {
+    "revision": "cd114ced01524b9bdcc7",
+    "url": "/yam-protocol/static/js/runtime-main.5df9ad51.js"
+  },
+  {
+    "revision": "85a013566c51273fd12ad9956054e906",
+    "url": "/yam-protocol/static/media/farmer.85a01356.png"
+  }
+]);
